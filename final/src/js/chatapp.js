7eb0e1f76a8dfe26ae65b24231cba05176d3c5c3
iagames <alex.games@gmail.com>
2023-04-12 09:03:14 -0700

    updating the documentation in the final js file

--- final/src/js/chatapp.js
+++ final/src/js/chatapp.js
@@ -7,21 +7,32 @@ window.chatWidgetApi.DIDLogin((loginResult)=>{
 
 
 function createPublicRoom() {
-let roomName = document.getElementById("pubroom").value;
-window.chatWidgetApi.createPublicRoom(roomName).then((roomID)=>{console.log(roomID)});
-//remove this alert once your are ready to create a public room
-alert("Create Public Room"+newpubroom);
+const roomName = document.getElementById("pubroom").value;
+if (roomName == null || roomName.length === 0 ){
+  alert("Please give the room a name");
+  return;
+}
+window.chatWidgetApi.createPublicRoom(roomName).then((roomID)=>{console.log(roomID)}).catch((error)=>{console.log(error)});
+//remove this alert once your are ready to create a public rooms
+alert("Public Room Created "+roomName);
 }
 
 function createPrivateRoom() {
-let newprivroom = document.getElementById("privroom").value;
-//remove this alert once your are ready to create a private room
-alert("Create Private Room");
+const roomName = document.getElementById("privroom").value;
+if (roomName == null || roomName.length === 0 ){
+  alert("Please give the room a name");
+  return;
+}
+window.chatWidgetApi.createPrivateRoom(roomName).then((roomID)=>{console.log(roomID)}).catch((error)=>{console.log(error)});;
+//remove this alert once your are ready to create a private rooms
+alert("Private Room Created "+roomName);
 } 
 
 function joinPrivateChatRoom() {
  //remove this alert once your are ready to join a private room
   alert("Join Private Chat Room");
+  //You can find the API for the sending.me widget here: https://sending-network.gitbook.io/sending.network/development/website-chat-widget-sdk/room
+  //This is the function that will be called when the user clicks the join button
   //window.chatWidgetApi.joinPrivateChatRoom(roomID).then((roomID)=>{
   //  console.log(roomID);
   //});
@@ -31,6 +42,8 @@ function inviteUser() {
   let invitee = document.getElementById("invitee").value;
   //remove this alert once your are ready to invite a user
   alert("Invite User"+invitee);
+  //You can find the API for the sending.me widget here: https://sending-network.gitbook.io/sending.network/development/website-chat-widget-sdk/room#invite-user-to-a-chat
+  //This is the function that will be called when the user clicks the invite button
   //window.chatWidget.createDM(userName).then((roomID)=>{console.log(roomID)})
     //console.log(roomID);
   //});
